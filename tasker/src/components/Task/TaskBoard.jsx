8f3f81dbda0ec9e0746b58170f4c018c755c248e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-05-23 18:10:59 +0600

    delete all tasks method

--- tasker/src/components/Task/TaskBoard.jsx
+++ tasker/src/components/Task/TaskBoard.jsx
@@ -57,6 +57,12 @@ const TaskBoard = () => {
     setTasks(remainingTasks);
   };
 
+  // handle all delete task
+  const handleAllTasksDelete = () => {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  };
+
   return (
     <section className="mb-20" id="tasks">
       {showAddTaskModal && (
@@ -73,7 +79,10 @@ const TaskBoard = () => {
         </div>
         {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddTaskHandler={() => setShowAddTaskModal(true)} />
+          <TaskActions
+            onAddTaskHandler={() => setShowAddTaskModal(true)}
+            onDeleteAllTaskHandler={handleAllTasksDelete}
+          />
 
           <TaskLists
             tasks={tasks}
