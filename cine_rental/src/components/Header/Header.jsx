ad51a75a5198fc06ff3ed55bdfa0d23b5f8ecec5
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-06-17 16:05:16 +0600

    dark mood

--- cine_rental/src/components/Header/Header.jsx
+++ cine_rental/src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import { useContext, useState } from "react";
 import Moon from "../../assets/icons/moon.svg";
+import Sun from "../../assets/icons/sun.svg";
 import Logo from "../../assets/logo.svg";
 import Ring from "../../assets/ring.svg";
 import ShoppingCart from "../../assets/shopping-cart.svg";
-import { MovieContext } from "../../contexts";
+import { MovieContext, ThemeContext } from "../../contexts";
 import CartDetails from "../Cart/CartDetails";
 
 const Header = () => {
   const [showCart, setShowCart] = useState(false);
 
   const { cartData } = useContext(MovieContext);
+  const { darkMood, setDarkMood } = useContext(ThemeContext);
 
   const handleCartShow = () => {
     setShowCart(true);
@@ -36,8 +38,14 @@ const Header = () => {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={() => setDarkMood((prev) => !prev)}
             >
-              <img src={Moon} width="24" height="24" alt="moon" />
+              <img
+                src={darkMood ? Moon : Sun}
+                width="24"
+                height="24"
+                alt="moon"
+              />
             </a>
           </li>
           <li>
